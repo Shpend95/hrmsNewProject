c0bf5ad9b023a537d5a013f9031216458557e7dd
Shpend95 <shpendpllana5@gmail.com>
2025-08-25 11:31:08 -0400

    updated regressionRunner

--- src/test/java/runners/RegressionRunner.java
+++ src/test/java/runners/RegressionRunner.java
@@ -2,7 +2,6 @@ package runners;
 
 import io.cucumber.junit.Cucumber;
 import io.cucumber.junit.CucumberOptions;
-import io.cucumber.testng.AbstractTestNGCucumberTests;
 import org.junit.runner.RunWith;
 
 @RunWith(Cucumber.class)
